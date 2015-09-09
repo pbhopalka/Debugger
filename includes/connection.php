ce3c4b276d431fc9a672f73da4b8c595667d5ae8
Piyush Bhopalka <pbhopalka@gmail.com>
2015-09-09 21:03:12 +0530

    Temporary removal of connection.php

--- includes/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once("config.php");
-	$mysqli = mysqli_connect($server, $sqlid, $sqlpass, $dbase);
-	if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-?>
